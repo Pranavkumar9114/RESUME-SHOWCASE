2df304ea79b64ab0bf0a4b0be690b76c55beaf97
Pranavkumar9114 <pranavkumar9114@gmail.com>
2025-07-13 23:23:04 +0530

    Update zip_operation.dart

--- wallfusion-flutter/wallfusion/lib/services/zip_operation.dart
+++ wallfusion-flutter/wallfusion/lib/services/zip_operation.dart
@@ -8,10 +8,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wallfusion/pages/splashscreen.dart';
 import 'package:wallfusion/services/internet_check_zipoperation.dart';
 
-// ignore: use_key_in_widget_constructors
 class ZipExtractorPage extends StatefulWidget {
   @override
-  // ignore: library_private_types_in_public_api
   _ZipExtractorPageState createState() => _ZipExtractorPageState();
 }
 
@@ -33,19 +31,16 @@ class _ZipExtractorPageState extends State<ZipExtractorPage> {
     try {
       if (!mounted) return;
       bool isConnected = await InternetCheckUtil.checkInternetConnection(context);
-      // ignore: avoid_print
       print("Internet connection status: $isConnected");
       if (isConnected) {
         await extractZipFromFirebase();
       } else {
-        // ignore: avoid_print
         print("No internet connection");
-        // Handle the case where there is no internet connection
-        // For example, you might want to show an error message or retry later
+
         updateStatusMessage("No internet connection. Please try again later.");
       }
     } catch (e) {
-      // ignore: avoid_print
+
       print("Error during internet check or processing: $e");
     }
   }
@@ -156,13 +151,13 @@ class _ZipExtractorPageState extends State<ZipExtractorPage> {
 
         if (tempFile.existsSync()) {
           await tempFile.delete();
-          // ignore: avoid_print
+    
           print('Deleted zip file: $tempPath/compress_image.zip');
         }
 
         if (tempDir.existsSync()) {
           await tempDir.delete(recursive: true);
-          // ignore: avoid_print
+  
           print('Temporary directory deleted: $tempPath');
         }
 
@@ -173,8 +168,7 @@ class _ZipExtractorPageState extends State<ZipExtractorPage> {
             _extractionDone = true;
           });
 
-          // Navigate to the splash screen or home page after extraction is done
-          // ignore: use_build_context_synchronously
+
           Navigator.of(context).pushReplacement(
               MaterialPageRoute(builder: (context) => const SplashScreen()));
         }
@@ -184,7 +178,7 @@ class _ZipExtractorPageState extends State<ZipExtractorPage> {
             _statusMessage = 'Error extracting zip file: $e';
           });
         }
-        // ignore: avoid_print
+
         print('Error extracting zip file: $e');
       }
     } else {
@@ -192,7 +186,7 @@ class _ZipExtractorPageState extends State<ZipExtractorPage> {
         setState(() {
           _extractionDone = true;
         });
-        // ignore: use_build_context_synchronously
+ 
         Navigator.of(context).pushReplacement(
             MaterialPageRoute(builder: (context) => const SplashScreen()));
       }
@@ -207,12 +201,12 @@ class _ZipExtractorPageState extends State<ZipExtractorPage> {
 
       if (zipFile.existsSync()) {
         await zipFile.delete();
-        // ignore: avoid_print
+  
         print(
             'Deleted zip file from internal storage: $internalStoragePath/files/compress_image.zip');
       }
     } catch (e) {
-      // ignore: avoid_print
+
       print('Error deleting zip file from internal storage: $e');
     }
   }
@@ -230,7 +224,7 @@ class _ZipExtractorPageState extends State<ZipExtractorPage> {
       Directory sourceDir = Directory(sourcePath);
       String internalStoragePath = await getInternalStoragePath();
       final destinationDir = Directory('$internalStoragePath/files');
-      // ignore: avoid_print
+
       print('Destination Directory Path: ${destinationDir.path}');
 
       if (!destinationDir.existsSync()) {
@@ -243,7 +237,7 @@ class _ZipExtractorPageState extends State<ZipExtractorPage> {
           String newPath =
               '${destinationDir.path}/${file.path.split('/').last}';
           await file.copy(newPath);
-          // ignore: avoid_print
+     
           print('Moved File Path: $newPath');
         } else if (file is Directory) {
           String newPath =
@@ -251,15 +245,15 @@ class _ZipExtractorPageState extends State<ZipExtractorPage> {
           Directory newDir = Directory(newPath);
           if (!newDir.existsSync()) {
             await newDir.create(recursive: true);
-            // ignore: avoid_print
+
             print('Created Directory Path: $newPath');
           }
         }
       }
     } catch (e) {
-      // ignore: avoid_print
+
       print('Error moving files: $e');
-      // ignore: use_rethrow_when_possible
+
       throw e;
     }
   }
@@ -268,13 +262,13 @@ class _ZipExtractorPageState extends State<ZipExtractorPage> {
     try {
       Directory? directory = await getExternalStorageDirectory();
       String path = directory!.path;
-      // ignore: avoid_print
+
       print('Internal Storage Path: $path');
       return path;
     } catch (e) {
-      // ignore: avoid_print
+ 
       print('Error getting internal storage path: $e');
-      // ignore: use_rethrow_when_possible
+ 
       throw e;
     }
   }
